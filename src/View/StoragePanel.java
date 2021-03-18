5f9beac188501ac70027b37c62e054a8d0243600
Alex Bergenholtz <alex.bergenholtz@hotmail.se>
2021-03-18 12:36:14 +0100

    test

--- src/View/StoragePanel.java
+++ src/View/StoragePanel.java
@@ -10,7 +10,7 @@ public class StoragePanel extends JPanel {
     public StoragePanel(){
         setLayout(new BorderLayout());
         table = new JTable(1000,2);
-        table.getColumnModel().getColumn(0).setHeaderValue("test");
+        table.getColumnModel().getColumn(0).setHeaderValue("tas");
         table.getColumnModel().getColumn(1).setHeaderValue("Mängd");
         scrollPane = new JScrollPane(table);
         add(scrollPane);
