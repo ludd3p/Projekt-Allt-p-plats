e028dd373f9909ecac674d31a9fb800fa0da8676
HElKh <h.elkhalil@hotmail.com>
2021-03-22 18:58:39 +0100

    + Order page är klar - Alla knappar

--- src/View/OrderPanel.java
+++ src/View/OrderPanel.java
@@ -1,7 +1,9 @@
 package View;
 
+import Model.Ingredient;
 import Model.Order;
 import Model.OrderItem;
+import Model.Unit;
 
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
@@ -13,8 +15,8 @@ public class OrderPanel extends JPanel {
     private JPanel leftPanel; // Vänstra panelen
     private JPanel rightPanel; // höger panelen
 
-    private JList<Order> orderJList; // Order history jlist
-    private List<Order> orderList; // Order history list
+    private JList<Order> orderHistoryJList; // Order history jlist
+    private List<Order> orderHistoryList; // Order history list
 
     private JList<OrderItem> currentOrderList; // Info om vald order
     private JButton showOrder; // Visa info om vald order
@@ -24,7 +26,7 @@ public class OrderPanel extends JPanel {
 
     public OrderPanel() {
         setLayout(new BorderLayout(5, 5));
-        orderList = new ArrayList<>();
+        orderHistoryList = new ArrayList<>();
 
         setupPanels();
     }
@@ -34,9 +36,9 @@ public class OrderPanel extends JPanel {
         rightPanel = new JPanel();
 
         leftPanel.setBorder(new TitledBorder("Order history"));
-        orderJList = new JList<>();
-        orderJList.setPreferredSize(new Dimension(500, 600));
-        leftPanel.add(orderJList);
+        orderHistoryJList = new JList<>();
+        orderHistoryJList.setPreferredSize(new Dimension(500, 600));
+        leftPanel.add(orderHistoryJList);
 
 
         rightPanel.setBorder(new TitledBorder("Info"));
@@ -50,8 +52,29 @@ public class OrderPanel extends JPanel {
         JPanel controlPanel = new JPanel();
         controlPanel.setBorder(new TitledBorder("Control panel"));
         showOrder = new JButton("Show order");
+        showOrder.addActionListener((e) -> {
+            if (orderHistoryJList.getSelectedValue() == null) {
+                JOptionPane.showConfirmDialog(null, "Du måste välja en order!", "ERROR", JOptionPane.DEFAULT_OPTION);
+                return;
+            }
+            orderPreview(orderHistoryJList.getSelectedValue());
+        });
         hasArrived = new JButton("Has arrived");
+        hasArrived.addActionListener((e -> {
+            if (orderHistoryJList.getSelectedValue() == null) {
+                JOptionPane.showConfirmDialog(null, "Du måste välja en order!", "ERROR", JOptionPane.DEFAULT_OPTION);
+                return;
+            }
+            orderHasArrived(orderHistoryJList.getSelectedValue());
+        }));
         remove = new JButton("Cancel order");
+        remove.addActionListener((e -> {
+            if (orderHistoryJList.getSelectedValue() == null) {
+                JOptionPane.showConfirmDialog(null, "Du måste välja en order att tabort!", "ERROR", JOptionPane.DEFAULT_OPTION);
+                return;
+            }
+            cancelOrder(orderHistoryJList.getSelectedValue());
+        }));
         controlPanel.add(showOrder);
         controlPanel.add(hasArrived);
         controlPanel.add(remove);
@@ -61,12 +84,46 @@ public class OrderPanel extends JPanel {
         this.add(rightPanel, BorderLayout.EAST);
 
 
+        Order order = new Order();
+        order.getOrderItems().add(new OrderItem(new Ingredient("test", Unit.AMOUNT), 10));
+        order.getOrderItems().add(new OrderItem(new Ingredient("test", Unit.AMOUNT), 10));
+        order.getOrderItems().add(new OrderItem(new Ingredient("test", Unit.AMOUNT), 10));
+        addOrderToHistory(order);
+        addOrderToHistory(order);
+        addOrderToHistory(order);
+        addOrderToHistory(order);
+
     }
 
-    public void addOrderToList(Order order) {
-        this.orderList.add(order);
-        Order[] val = new Order[orderList.size()];
-        orderList.toArray(val);
-        this.orderJList.setListData(val);
+    public void addOrderToHistory(Order order) {
+        this.orderHistoryList.add(order);
+        Order[] val = new Order[orderHistoryList.size()];
+        orderHistoryList.toArray(val);
+        this.orderHistoryJList.setListData(val);
+    }
+
+    public void orderPreview(Order order) {
+        if (order == null)
+            order = new Order();
+        OrderItem[] items = new OrderItem[order.getOrderItems().size()];
+        order.getOrderItems().toArray(items);
+        currentOrderList.setListData(items);
+    }
+
+    public void orderHasArrived(Order order) {
+        //TODO Fyll på lagret
+        this.orderHistoryList.remove(order);
+        Order[] val = new Order[orderHistoryList.size()];
+        orderHistoryList.toArray(val);
+        this.orderHistoryJList.setListData(val);
+        orderPreview(null);
+    }
+
+    public void cancelOrder(Order order) {
+        orderPreview(null);
+        this.orderHistoryList.remove(order);
+        Order[] val = new Order[orderHistoryList.size()];
+        orderHistoryList.toArray(val);
+        this.orderHistoryJList.setListData(val);
     }
 }
