1f5a07292d417c8c7ed0bbadd79b5a953826c38d
ZhinkyPinky <jonathan.engstrom@gmail.com>
2021-03-26 12:52:45 +0100

    +La till enheter i Unit klassen, samt en metod för att hämta en array av prefixes.

--- src/Model/Unit.java
+++ src/Model/Unit.java
@@ -4,7 +4,14 @@ package Model;
 // Exemepl en order på mjöl läggs följade: 500 gram mjöl meddans
 // en order på mjölk räknas i 5 st mjölk packet.
 public enum Unit {
-    WEIGHT("g"), AMOUNT("st");
+    MILLILITRE("mL"),
+    DECILITRE("dL"),
+    LITRE("L"),
+    MILLIGRAM("mg"),
+    GRAM("g"),
+    KILOGRAM("kg"),
+    WEIGHT("g"),
+    AMOUNT("st");
 
     private String prefix;
 
@@ -18,5 +25,15 @@ public enum Unit {
 
     public void setPrefix(String prefix) {
         this.prefix = prefix;
+    }
+
+    public static String[] getPrefixArray(){
+        String[] prefixArray = new String[values().length];
+
+        for(int i = 0; i < prefixArray.length; i++){
+            prefixArray[i] = values()[i].prefix;
+        }
+
+        return prefixArray;
     }
 }
